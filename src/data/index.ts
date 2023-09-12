e6ee32056479327a5b8e5e4bd4c10b1db5565797
saultharwat74 <mohamedtharout190@gmail.com>
2023-09-12 19:17:32 +0300

    feat: added geonameid, emojis and timezone to countries

--- src/data/index.ts
+++ src/data/index.ts
@@ -2,4 +2,6 @@ export { default as continents } from "./continents";
 export { default as languages } from "./languages";
 export { default as countries } from "./countries";
 export { default as currencies } from "./currencies";
-export { default as countryCode } from "./countryCode"
+export { default as countryCode } from "./countryCode";
+export { default as geonameId } from "./geonameId";
+export { default as emojis } from "./emojis";
